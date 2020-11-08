20767e63ca27860d32fcdbad3a56ab067f98bd62
Nino Filiu <nino.filiu@gmail.com>
2020-11-08 14:37:44 +0100

    wip

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,31 @@
 <template>
-  <div/>
+  <div class="app">
+    <div class="controls"/>
+    <div class="display"/>
+  </div>
 </template>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+</style>
+
+<style lang="scss" scoped>
+.app {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+}
+.controls {
+  background-color: grey;
+  width: 30%;
+}
+.display {
+  background-color: darkgrey;
+  width: 70%;
+}
+</style>
