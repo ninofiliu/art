31efb032972fd9b076c43fda974fd48b997ddd51
Nino Filiu <nino.filiu@gmail.com>
2023-02-26 22:51:31 +0100

    use audio buffer

--- src/vocoder.ts
+++ src/vocoder.ts
@@ -2,4 +2,15 @@ import getAudioContext from "./shared/getAudioContext";
 
 export default async () => {
   const ac = await getAudioContext();
+  await ac.resume();
+
+  const resp = await fetch("/music/2021.mp3");
+  const arrayBuffer = await resp.arrayBuffer();
+  const audioBuffer = await ac.decodeAudioData(arrayBuffer);
+  const source = ac.createBufferSource();
+  source.buffer = audioBuffer;
+
+  source.connect(ac.destination);
+  source.loop = true;
+  source.start();
 };
