fdec605f8b89be3e46a5f7378e696cd89629c821
Nino Filiu <2unfshwpu@mozmail.com>
2025-03-21 01:47:18 +0100

    add textures support

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -3,9 +3,14 @@ precision highp float;
 out vec4 fragColor;
 
 uniform vec3 iResolution;
+uniform sampler2D tex0;
+uniform sampler2D tex1;
 
 void main() {
+  vec2 uv = gl_FragCoord.xy / iResolution.xy;
+  uv.y = 1. - uv.y;
   float x = gl_FragCoord.x / iResolution.x;
   float y = gl_FragCoord.y / iResolution.y;
-  fragColor = vec4(x, y, 0.0, 1.0);
+  // fragColor = vec4(x, y, 0.0, 1.0);
+  fragColor = texture(tex1, uv);
 }
--- src/webgl.ts
+++ src/webgl.ts
@@ -82,13 +82,54 @@ gl.clear(gl.COLOR_BUFFER_BIT);
 
 const uniforms = {
   iResolution: gl.getUniformLocation(shaderProgram, "iResolution"),
+  tex0: gl.getUniformLocation(shaderProgram, "tex0"),
+  tex1: gl.getUniformLocation(shaderProgram, "tex1"),
 };
 
+gl.uniform1i(uniforms.tex0, 0);
+gl.uniform1i(uniforms.tex1, 1);
+
+const createTexture = (gl: WebGL2RenderingContext, index: number) => {
+  const texture = gl.createTexture()!;
+  gl.activeTexture(gl.TEXTURE0 + index);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.bindTexture(gl.TEXTURE_2D, null);
+  return texture;
+};
+
+const updateTexture = (
+  gl: WebGL2RenderingContext,
+  texture: WebGLTexture,
+  image: HTMLImageElement
+): void => {
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  gl.bindTexture(gl.TEXTURE_2D, null);
+};
+
+const textures = Array(2)
+  .fill(null)
+  .map((_, i) => createTexture(gl, i));
+["/yellow_teeth.jpg", "/bug3.avif"].forEach((src, i) => {
+  const img = document.createElement("img");
+  img.src = src;
+  img.addEventListener("load", () => {
+    updateTexture(gl, textures[i], img);
+  });
+});
+
 const render = () => {
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-
   gl.uniform3f(uniforms.iResolution, canvas.width, canvas.height, 0);
+  textures.forEach((t, i) => {
+    gl.activeTexture(gl.TEXTURE0 + i);
+    gl.bindTexture(gl.TEXTURE_2D, t);
+  });
 
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
   requestAnimationFrame(render);
 };
 render();
