479343e588130638a957e2ac9f25fcb3b54d7148
Nino Filiu <2unfshwpu@mozmail.com>
2026-01-23 22:31:47 +0100

    few outputs

--- src/bunnymosh/conv.ts
+++ src/bunnymosh/conv.ts
@@ -10,9 +10,10 @@ import {
   UrlSource,
 } from "mediabunny";
 
+// ffmpeg.exe -i .\highway2_5s.mp4 -profile:v baseline .\highway2_5s.baseline.mp4
 const input = new Input({
   formats: ALL_FORMATS,
-  source: new UrlSource("/highway2_5s.mp4"),
+  source: new UrlSource("/indian.baseline.mp4"),
 });
 
 const track = await input.getPrimaryVideoTrack();
@@ -35,18 +36,29 @@ const pkts = [] as EncodedPacket[];
 for await (const pkt of sink.packets()) {
   pkts.push(pkt);
 }
-console.log(pkts);
+console.log(pkts.map((pkt) => pkt.sequenceNumber));
+console.log(
+  pkts
+    .toSorted((a, b) => a.timestamp - b.timestamp)
+    .map((pkt) => pkt.sequenceNumber)
+);
 const fps = 1 / pkts[0].duration;
 
 console.log(pkts.length);
 const repkts = [
-  ...Array(1)
+  // ...Array(1)
+  //   .fill(null)
+  //   .flatMap(() => pkts.slice(0, 20)),
+  ...pkts.slice(0, 100),
+  ...Array(Math.floor((349 - 100) * 2))
     .fill(null)
-    .flatMap(() => pkts.slice(0, 52)),
-  ...Array(20)
-    .fill(null)
-    .flatMap(() => pkts.slice(1, 50)),
+    .map((_, i) => pkts[Math.floor(100 + i / 2)])
+    .filter((pkt, i) => i == 0 || pkt.type == "delta"),
+  // ...Array(100)
+  //   .fill(null)
+  //   .flatMap(() => pkts.slice(20, 21)),
 ].map((pkt, i) => new EncodedPacket(pkt.data, pkt.type, i / fps, pkt.duration));
+console.log(repkts.length);
 
 for (const pkt of repkts) {
   await source.add(pkt, { decoderConfig });
@@ -61,5 +73,7 @@ video.autoplay = true;
 video.loop = true;
 video.controls = true;
 video.style.display = "block";
-video.style.width = "100%";
+video.style.width = "100vw";
+video.style.height = "100vh";
+video.style.objectFit = "contain";
 document.body.append(video);
